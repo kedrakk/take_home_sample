af84bf878487743a1ef4e2e0d7cc698842fc523b
kedrakk <keaindrak@gmail.com>
2022-06-06 18:37:48 +0630

    change gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:take_home/pages/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
