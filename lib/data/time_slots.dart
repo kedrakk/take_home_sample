6c8dc895dee76899956b52cfd1cafc740663ea25
kedrakk <keaindrak@gmail.com>
2022-06-07 10:10:00 +0630

    remove unused

--- lib/data/time_slots.dart
+++ lib/data/time_slots.dart
@@ -22,49 +22,6 @@ class TimeSlot {
       slots: tempSlots,
     );
   }
-
-  static List<TimeSlot> timeSlots = <TimeSlot>[
-    TimeSlot(
-      date: "2022-06-15",
-      slots: <Slots>[
-        Slots(
-          startTime: "2022-06-15 09:15",
-          endTime: "2022-06-15 09:30",
-          available: false,
-        ),
-        Slots(
-          startTime: "2022-06-15 09:30",
-          endTime: "2022-06-15 09:45",
-          available: true,
-        ),
-        Slots(
-          startTime: "2022-06-15 09:45",
-          endTime: "2022-06-15 10:00",
-          available: true,
-        ),
-      ],
-    ),
-    TimeSlot(
-      date: "2022-06-16",
-      slots: <Slots>[
-        Slots(
-          startTime: "2022-06-16 10:00",
-          endTime: "2022-06-16 10:15",
-          available: true,
-        ),
-        Slots(
-          startTime: "2022-06-16 10:15",
-          endTime: "2022-06-16 10:30",
-          available: true,
-        ),
-        Slots(
-          startTime: "2022-06-16 10:30",
-          endTime: "2022-06-16 10:45",
-          available: true,
-        ),
-      ],
-    ),
-  ];
 }
 
 class Slots {
