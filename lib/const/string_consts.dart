fe310a64c41576df093a6c53fce994485b67cb37
kedrakk <keaindrak@gmail.com>
2022-06-11 09:09:09 +0630

    add new url

--- lib/const/string_consts.dart
+++ lib/const/string_consts.dart
@@ -1,4 +1,5 @@
-const mockURL = "https://a69648a5-f361-43d0-a18c-5e0c6942687b.mock.pstmn.io";
+const mockURLOld = "https://a69648a5-f361-43d0-a18c-5e0c6942687b.mock.pstmn.io";
+const mockURL = "https://406860e9-1877-4509-a313-3e050c0b704c.mock.pstmn.io";
 
 class Apis {
   static const String getSlots = "$mockURL/appointments/getSlots";
